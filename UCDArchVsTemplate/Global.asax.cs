861c719c496cd5f97dc1eb038d05a5431af2506e
Scott Kirkland <srkirkland@gmail.com>
2010-03-25 16:52:03 -0700

    hooked up the Url conventions and web.config registrations

--- UCDArchVsTemplate/Global.asax.cs
+++ UCDArchVsTemplate/Global.asax.cs
@@ -1,5 +1,12 @@
 ﻿using System.Web.Mvc;
 using System.Web.Routing;
+using Castle.Windsor;
+using Microsoft.Practices.ServiceLocation;
+using MvcContrib.Castle;
+using UCDArch.Web.IoC;
+using UCDArch.Web.ModelBinder;
+using UCDArch.Web.Validator;
+using UCDArchVsTemplate.Controllers;
 
 namespace UCDArchVsTemplate
 {
@@ -10,9 +17,32 @@ namespace UCDArchVsTemplate
     {
         protected void Application_Start()
         {
+            #if DEBUG
+            HibernatingRhinos.NHibernate.Profiler.Appender.NHibernateProfiler.Initialize();
+            #endif
+
+            xVal.ActiveRuleProviders.Providers.Add(new ValidatorRulesProvider());
+
+            ModelBinders.Binders.DefaultBinder = new UCDArchModelBinder();
+
+            InitializeServiceLocator();
+
             RouteRegistrar.RegisterRoutesTo(RouteTable.Routes);
 
             AreaRegistration.RegisterAllAreas();
+        }
+
+        private static void InitializeServiceLocator()
+        {
+            IWindsorContainer container = new WindsorContainer();
+            ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container));
+
+            container.RegisterControllers(typeof(HomeController).Assembly);
+            ComponentRegistrar.AddComponentsTo(container);
+
+            ServiceLocator.SetLocatorProvider(() => new WindsorServiceLocator(container));
+
+            return;
         }
     }
 }
